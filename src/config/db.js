9e66f0e67c713855cca2dda47c7e5ec3cd0bfc9c
saaqes <jramirezceballos27@gmail.com>
2026-03-20 06:37:14 -0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,8 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL + '&sslmode=verify-full',
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
